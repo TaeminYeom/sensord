5934d9ea1345ca495e2d9b9f42c7fc2cbf0a0eac
akhilkedia94 <akhil.kedia@samsung.com>
2016-08-16 20:33:00 +0900

    Magnetic RV Sensor
    Change-Id: I7a0586952f69404573d5ae974d6dc3c8cf1b7055
    Signed-off-by: akhilkedia94 <akhil.kedia@samsung.com>

--- src/server/sensor_loader.cpp
+++ src/server/sensor_loader.cpp
@@ -47,6 +47,7 @@
 #endif
 #ifdef ENABLE_ROTATION_VECTOR
 #include <rv_sensor.h>
+#include <magnetic_rv_sensor.h>
 #endif
 
 using std::vector;
@@ -172,6 +173,7 @@ void sensor_loader::create_sensors(void)
 #endif
 #ifdef ENABLE_ROTATION_VECTOR
 	create_virtual_sensors<rv_sensor>("Rotation Vector");
+	create_virtual_sensors<magnetic_rv_sensor>("Magnetic Rotation Vector");
 #endif
 #ifdef ENABLE_GRAVITY
 	create_virtual_sensors<gravity_sensor>("Gravity");
