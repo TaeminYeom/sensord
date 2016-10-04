66e67ca071a6b328503340bbef97cc018c191b9b
hs81.go <hs81.go@samsung.com>
2016-10-04 10:46:41 +0900

    sensord: remove an useless event suffix name
    Change-Id: Ia16741513429196bdbb1063e9a7dcdce62f8a41b
    Signed-off-by: hs81.go <hs81.go@samsung.com>

--- src/client/client_common.cpp
+++ src/client/client_common.cpp
@@ -42,9 +42,8 @@ const char* get_sensor_name(sensor_id_t id)
 const char* get_event_name(unsigned int event_type)
 {
 	sensor_type_t type = (sensor_type_t) (event_type >> SENSOR_EVENT_SHIFT);
-	std::string name(util_sensor_type_t::get_string(type));
 
-	return name.append("_EVENT").c_str();
+	return util_sensor_type_t::get_string(type);
 }
 
 unsigned int get_calibration_event_type(unsigned int event_type)
