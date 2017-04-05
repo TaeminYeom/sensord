98bc10f25673e914b73f571e5c0bc984f10b8624
kibak.yoon <kibak.yoon@samsung.com>
2017-04-05 19:54:34 +0900

    sensord: add sensor_publisher and sensor_observer interfaces
    - observer pattern
    
    Change-Id: I7c0f859a48faca21f1cf51fede816cc84c9b016e
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

---
+++ src/server/sensor_observer.h
@@ -0,0 +1,37 @@
+/*
+ * sensord
+ *
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __SENSOR_OBSERVER_H__
+#define __SENSOR_OBSERVER_H__
+
+#include <message.h>
+
+namespace sensor {
+
+class sensor_observer {
+public:
+	virtual ~sensor_observer() {}
+
+	/* for performance, use message */
+	virtual int update(const char *uri, ipc::message *msg) = 0;
+};
+
+}
+
+#endif /* __SENSOR_OBSERVER_H__ */
