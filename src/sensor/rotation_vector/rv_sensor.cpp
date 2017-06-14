866a049c502fc49ec750e02048cecb2e3fbe57ea
kibak.yoon <kibak.yoon@samsung.com>
2017-06-14 20:17:15 +0900

    sensord: rv: fix bug that the accuracy value is not changed
    Change-Id: I9a3020ae8e748e09dbb79e0e2f13cc8c3208d65d
    Signed-off-by: kibak.yoon <kibak.yoon@samsung.com>

--- src/sensor/rotation_vector/rv_sensor.cpp
+++ src/sensor/rotation_vector/rv_sensor.cpp
@@ -93,9 +93,6 @@ int rv_sensor::update(uint32_t id, sensor_data_t *data, int len)
 	else if (id == SRC_ID_GYRO)
 		m_fusion.push_gyro(*data);
 
-	if (m_accuracy > data->accuracy)
-		m_accuracy = data->accuracy;
-
 	if (!m_fusion.get_rv(timestamp, m_w, m_x, m_y, m_z))
 		return OP_ERROR;
 
@@ -103,6 +100,7 @@ int rv_sensor::update(uint32_t id, sensor_data_t *data, int len)
 		return OP_ERROR;
 
 	m_time = timestamp;
+	m_accuracy = data->accuracy;
 
 	_D("[rotation_vector] : [%10f] [%10f] [%10f] [%10f]", m_x, m_y, m_z, m_w);
 	return OP_SUCCESS;
